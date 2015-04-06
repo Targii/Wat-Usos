ac931df5fed17ff76f947b904b587f2979ac0d7c
Targii <adamprefix@gmail.com>
2015-04-06 23:19:58 +0200

    This is Adam

--- app/src/main/java/com/example/watusos/watusos/MainActivity.java
+++ app/src/main/java/com/example/watusos/watusos/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         //This is Targiiaaaaaaaa
-        //This is Adam
+        //This is Adamm
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
